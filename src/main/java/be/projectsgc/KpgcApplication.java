7573aaa45311c1d43b165d45b97a9e9fa75683cd
Gert <gert.goetstouwers@gmail.com>
2017-04-23 11:14:36 +0200

    init project

---
+++ src/main/java/be/projectsgc/KpgcApplication.java
@@ -0,0 +1,12 @@
+package be.projectsgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpgcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpgcApplication.class, args);
+	}
+}
